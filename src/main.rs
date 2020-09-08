bc5ec0828fd10d0b7af3934657ae5a4d4ceb8b4b
thesunRider <suryasaradhi3@gmail.com>
2020-09-08 14:40:35 -0400

    Added Small changes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 #[macro_use]
 extern crate clap;
+
 use clap::App;
 use std::fs;
 
 fn main()  {
 	let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
-
+    println!("{:?}",matches );
     let password = matches.value_of("password").unwrap_or("pass");
     println!("This is your password : {}", password);
 
